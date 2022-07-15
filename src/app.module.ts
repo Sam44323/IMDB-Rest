41c1ed8390572cc085685abc194450d2ed14b7bc
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-16 01:59:46 +0530

    aadd: added custom configs for model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ImdbModule } from './imdb/imdb.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ImdbModule, MongooseModule.forRoot(process.env.MONGO_URI)],
+  imports: [
+    ImdbModule,
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_URI),
+  ],
 })
 export class AppModule {}
