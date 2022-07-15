b32c23a8257ad7e805da2b0b0b8d2ee5db1ac0af
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-16 01:58:57 +0530

    add: added initiater for mongoose-model

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ImdbModule } from './imdb/imdb.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ImdbModule],
+  imports: [ImdbModule, MongooseModule.forRoot(process.env.MONGO_URI)],
 })
 export class AppModule {}
