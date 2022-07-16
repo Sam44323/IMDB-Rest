278d4a01beb00a591f7727cd05bcc86e6e01c1a1
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-16 21:40:41 +0530

    add: added option for movie-details data

--- src/imdb/imdb.service.ts
+++ src/imdb/imdb.service.ts
@@ -1,9 +1,44 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException, HttpService } from '@nestjs/common';
 import { IMDB } from './imdb.model';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
+import { lastValueFrom, map } from 'rxjs';
 
 @Injectable()
 export class ImdbService {
-  constructor(@InjectModel('IMDB') private readonly imdbModel: Model<IMDB>) {}
+  constructor(
+    @InjectModel('IMDB') private readonly imdbModel: Model<IMDB>,
+    private readonly http: HttpService,
+  ) {}
+
+  async getMovieData(title?: string, imdb_id?: string) {
+    if (!title && !imdb_id) {
+      throw new NotFoundException('No title or imdb_id provided');
+    }
+    const dataExists = await this.imdbModel.findOne(
+      title ? { title } : { imdb_id },
+    );
+    if (dataExists) {
+      return dataExists;
+    }
+    const url = `http://www.omdbapi.com/?apikey=${process.env.OMDB_API_KEY}`;
+    const params = {};
+    if (title) {
+      params['t'] = title;
+    } else if (imdb_id) {
+      params['i'] = imdb_id;
+    }
+    const response: any = await lastValueFrom(
+      this.http.get(url, { params }).pipe(map((resp) => resp.data)),
+    );
+    await this.imdbModel.create({
+      imdb_id: response.imdbID,
+      title: response.Title,
+      rating: response.imdbRating,
+      year: response.Year,
+      details: response.Plot,
+    });
+    console.log(response);
+    return response;
+  }
 }
