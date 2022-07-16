c9b32c821e55c4323deafc975f3de1e755d5c374
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-16 21:27:05 +0530

    update: updated the example.env file

--- src/imdb/imdb.service.ts
+++ src/imdb/imdb.service.ts
@@ -1,4 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { IMDB } from './imdb.model';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
-export class ImdbService {}
+export class ImdbService {
+  constructor(@InjectModel('IMDB') private readonly imdbModel: Model<IMDB>) {}
+}
