c069f9dd11735fb8643b179b3ff48e0371abfd5b
Suranjan <suranjanbiswas2002@gmail.com>
2022-07-16 21:17:24 +0530

    aadd: added support for mongoose-injectors

--- src/imdb/imdb.module.ts
+++ src/imdb/imdb.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ImdbController } from './imdb.controller';
 import { ImdbService } from './imdb.service';
 
+import { IMDBSchema } from './imdb.model';
+
 @Module({
+  imports: [MongooseModule.forFeature([{ name: 'IMDB', schema: IMDBSchema }])],
   controllers: [ImdbController],
   providers: [ImdbService],
 })
